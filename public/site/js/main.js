d34572b7507cbc96424e9365315128f55a1a141d
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-05-07 18:55:57 +0200

    added public folder

---
+++ public/site/js/main.js
@@ -0,0 +1,10 @@
+function hideBoot() {
+    document.querySelector('.circle_wrapper').style.top = '-200vh'
+    setTimeout(function () {
+        document.querySelector('.circle_wrapper').outerHTML = ''
+    }, 2500);
+}
+
+setTimeout(function () {
+    hideBoot();
+}, 2);
